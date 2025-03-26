73276feece4776dca6bf9556e554d85746c53a9b
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2025-03-26 15:38:14 -0300

    hello java

---
+++ hello java.java
@@ -0,0 +1,10 @@
+/******************************************************************************
+
+Programa hello.java
+
+*******************************************************************************/
+public class hello {
+public static void main (String[] args){
+    System.out.print("Hello java");
+ }    
+}
