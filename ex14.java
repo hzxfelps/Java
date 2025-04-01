c602b82d994e5781782bfd78dc48f2623de5ab55
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2025-03-31 23:57:12 -0300

    ex14 - repetiçao 10 vezes de eu gosto de estudar algoritmos

---
+++ ex14.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+
+exercicio 14 github
+ Escreva um programa que exiba 10 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
+
+*******************************************************************************/
+public class ex14 {
+
+    public static void main(String[] args) {
+       for (int i = 1; i <= 10; i++){
+           System.out.println("Eu gosto de estudar Algoritmos!");
+       }
+    }
+}
