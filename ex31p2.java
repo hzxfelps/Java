26b8c6335cca0b6a51d834e4c6a3501cd379406d
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2025-04-07 13:37:25 -0300

    ex31p2- mostra numeros de 1 a 20 para o usuario no sentido vertical, e logo em seguida mostra um do lado do outro horizontalmente

---
+++ ex31p2.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ex.31
+Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro.
+parte 2
+
+*******************************************************************************/
+public class ex31p2
+ {
+ 	public static void main(String[] args) {
+ 	    int numero = 0;
+ 	while(numero <= 19){
+ 	    numero++;
+ 	    System.out.print (numero+ "º");
+ 	    
+ 	}
+ 	
+ 	}
+ }
