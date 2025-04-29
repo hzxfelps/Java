c2e0f84378a0efdf715303004ae79eb93c953b84
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2025-04-28 21:47:09 -0300

    exloop6 - Soma dos numeros pares de 1 a 100 (for)

---
+++ exloop6.java
@@ -0,0 +1,18 @@
+/****************************************************************************** 
+6.   
+Faça um programa que calcule e exiba a soma dos números pares de 1 a 100 utilizando um laço de repetição. Utilize for.
+*******************************************************************************/
+import java.util.Scanner;
+
+public class exloop6 {
+    public static void main(String[] args) {
+        int somanumpar = 0;
+
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 == 0){
+            somanumpar += i
+            }
+        }
+        System.out.println("A soma dos números pares de 1 a 100 é: " + somanumpar);
+    }
+}
