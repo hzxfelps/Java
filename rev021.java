8017ec4f9fa9fa94d45364d71103f2bf3b79e5c6
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2025-05-05 23:06:23 -0300

    Imprime numeros dentro de um array

---
+++ rev021.java
@@ -0,0 +1,21 @@
+/*
+Exercicio 021- Crie um vetor de10 inteiros e inicialize-o 
+com valores sequenciais (1, 2, 3, ..., 10). Em seguida, 
+imprima todos os valores do vetor na tela.
+*/
+import java.util.Scanner;
+public class rev021
+{
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+    int [] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+   
+    for( int i = 0; i < 10; i++) {
+    System.out.print("\n"+num[i]);
+    
+	    
+}
+	}
+	}
+
+
