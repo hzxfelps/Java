89dc4d04171882c359651c63803f45aa0aff51cf
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2026-01-28 23:50:20 -0300

    Atualizar o Animal.java

--- Revisao-ex11to20/src/Animal.java
+++ Revisao-ex11to20/src/Animal.java
@@ -1,16 +1,7 @@
 public class Animal {
     String nome;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public void fazerSom() {
-        System.out.println("O ");
+        System.out.println("O animal faz um som");
     }
-
 }
